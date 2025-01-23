a5f08fff2af9aca8e6d285f067befb68391fdd92
FranklinS734 <martinc3@usf.edu>
2025-01-22 23:41:25 -0500

    Task 1 - Employee Information.

---
+++ cc_1.js
@@ -0,0 +1,8 @@
+// Task 1: Employee Information
+let employeeName = "Adam Man";
+const employeeID = 75608;
+var isActive = true;
+
+console.log("Employee Name:", employeeName, "Type:", typeof employeeName);
+console.log("Employee ID:", employeeID, "Type:", typeof employeeID);
+console.log("Is Active:", isActive, "Type:", typeof isActive);
