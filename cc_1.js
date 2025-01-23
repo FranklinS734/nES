0fd7aefd928391d87c71792f5b60afdfeb1905ab
FranklinS734 <martinc3@usf.edu>
2025-01-23 01:00:04 -0500

    Task 2 - Product Details

--- cc_1.js
+++ cc_1.js
@@ -6,3 +6,19 @@ var isActive = true;
 console.log("Employee Name:", employeeName, "Type:", typeof employeeName);
 console.log("Employee ID:", employeeID, "Type:", typeof employeeID);
 console.log("Is Active:", isActive, "Type:", typeof isActive);
+
+// Task 2: Prodcuct Details
+// Declare a variable for the name of the product
+let productName = "Earbuds";
+//Stating a constant for the price of the product
+const productPrice = 159.99;
+// Declare a variable to state the product is available
+var isAvailable = true;
+//Showing the product's name and its data type
+console.log("Product Name:", productName, "Type:", typeof productName); 
+
+// Output the product's price and its data type
+console.log("Product Price:", productPrice, "Type:", typeof productPrice); 
+
+// Output whether the product is available and its data type
+console.log("Is Available:", isAvailable, "Type:", typeof isAvailable); 
